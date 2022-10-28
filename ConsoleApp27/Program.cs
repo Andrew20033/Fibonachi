d533919ab7cdcb6718d0fed2b3a3cf4c3ff92857
Stuk_uc23@student.itstep.org <Stuk_uc23@student.itstep.org>
2022-10-28 18:13:09 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp27/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fibonachi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Xf = 0; 
+            Console.WriteLine("Введите число, которое необходимо исследовать.\n");
+            Xf = Console.Read();
+            int Xi_2 = 0, Xi_1 = 1; 
+            int Xi = 0;
+            while (true)
+            {
+                Xi = Xi_1 + Xi_2; 
+                Xi_2 = Xi_1;       
+                Xi_1 = Xi;        
+                if ((Xf == Xi) || (Xf == 0))
+                {
+                    Console.WriteLine("Введенное число принадлежит последовательности Фибоначчи\n");
+                    break;
+                }
+                else if (Xf < Xi)
+                {
+                    Console.WriteLine("Введенное число не принадлежит последовательности Фибоначчи\n");
+                    break;
+                }
+            }
+        }
+    }
+}
